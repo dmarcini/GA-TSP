af049fabd96d5ee1ff63ae8672d8cb181d64c389
Damian Marciniak <damian.marciniak.1996@gmail.com>
2021-01-25 21:17:43 +0100

    Add Params class

---
+++ include/params.hpp
@@ -0,0 +1,18 @@
+#ifndef PARAMS_HPP_
+#define PARAMS_HPP_
+
+
+class Params
+{
+public:
+    static int iterations_number;
+
+    static int population_size;
+    // crossover probability
+    static double pk;
+    // mutation probability
+    static double pm;
+};
+
+
+#endif // PARAMS_HPP_
---
+++ src/params.cpp
@@ -0,0 +1,8 @@
+#include "params.hpp"
+
+
+int Params::iterations_number = 1000;
+
+int Params::population_size = 250;
+double Params::pk = 0.7;
+double Params::pm = 0.1;
